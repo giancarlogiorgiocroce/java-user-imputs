b38fc8cd6f9b1095b467f9e832e8aef28b8cc57c
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-10-19 17:23:03 +0200

    Sei Robb!

--- QuickStart.java
+++ QuickStart.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 class QuickStart {
     public static void main (String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("My name is Giancarlo G. Croce.");
+        // Openin the gate to find the user imputs.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.printf("You are %s! What a stupid name!", name);
+
+        // If not closed, the scanner keep listening open wide to all forms of hijacking.
+        scanner.close();
     }
+    
 }
